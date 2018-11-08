39da877a9ec6b0674135c763026f53b12b6b3d86
Long DT <longtd@hiworld.com.vn>
2018-11-08 17:39:27 +0700

    Create Project

---
+++ QRScanner/QRScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by Long DT on 8/11/2561 BE.
+//  Copyright © 2561 Long DT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
